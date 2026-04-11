74750807fefd7a79594ec4d3837701e04e2d2b52
patrik121212 <119336295+patrik121212@users.noreply.github.com>
2026-04-12 01:47:41 +0800

    Add Transfer model with fillable properties and relationships to User and Wallet

--- app/Models/Transfer.php
+++ app/Models/Transfer.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Wallet;
 
 class Transfer extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'from_wallet_id',
+        'to_wallet_id',
+        'amount',
+        'description',
+        'transfer_date'
+    ];
+
+    // ke user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //  wallet asal
+    public function fromWallet()
+    {
+        return $this->belongsTo(Wallet::class, 'from_wallet_id');
+    }
+
+    //  wallet tujuan
+    public function toWallet()
+    {
+        return $this->belongsTo(Wallet::class, 'to_wallet_id');
+    }
 }
